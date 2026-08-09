4718025a4b73e81527bd06078d48579f48dee04c
오봉역 입환게임 <sms6171@gmail.com>
2026-08-09 19:21:59 +0900

    배포 v47

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 //
 // 주의: 자산을 추가했다면 아래 CACHE 이름의 숫자를 올려야 이전 캐시가 정리된다.
 
-const CACHE = 'obong-game-v46';
+const CACHE = 'obong-game-v47';
 
 const PRECACHE = [
   './',
@@ -34,6 +34,7 @@ const PRECACHE = [
   './assets/worker/back1.png',
   './assets/worker/back2.png',
   './assets/worker/wave.png',
+  './assets/radio.png',
   './assets/cursor-hand.png',
   './assets/cursor-hand-active.png',
   './assets/worker-main/walk1.png',
